9ca0b98dc2106bd485663e17bbc187f89f2328f0
msaamuks-tech <msaamuks@gmail.com>
2026-07-20 20:04:35 +0500

    Update register.js

--- js/register.js
+++ js/register.js
@@ -4,6 +4,29 @@ form.addEventListener("submit", function(e) {
 
     e.preventDefault();
 
-    alert("Работает!");
+    const inputs = document.querySelectorAll("input");
+
+    const username = inputs[0].value;
+    const email = inputs[1].value;
+    const password = inputs[2].value;
+    const repeatPassword = inputs[3].value;
+
+    if (!username || !email || !password || !repeatPassword) {
+        alert("Заполни все поля");
+        return;
+    }
+
+    if (password !== repeatPassword) {
+        alert("Пароли не совпадают");
+        return;
+    }
+
+    localStorage.setItem("username", username);
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
+
+    alert("Аккаунт создан!");
+
+    window.location.href = "login.html";
 
 });
