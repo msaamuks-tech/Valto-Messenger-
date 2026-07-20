ead3c6e4f6faf4259775b96ea54e265d73138635
msaamuks-tech <msaamuks@gmail.com>
2026-07-20 21:21:15 +0500

    Create profile.js

---
+++ js/profile.js
@@ -0,0 +1,18 @@
+const username = localStorage.getItem("username");
+const email = localStorage.getItem("email");
+
+
+document.getElementById("profileName").textContent = username || "Гость";
+
+document.getElementById("profileEmail").textContent = email || "Нет email";
+
+
+document.getElementById("logout").addEventListener("click", function(){
+
+    localStorage.clear();
+
+    alert("Вы вышли из аккаунта");
+
+    window.location.href = "index.html";
+
+});
