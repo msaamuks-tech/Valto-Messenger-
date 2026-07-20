4a623374b89a352a08191a5c1d477ff88b7653bc
msaamuks-tech <msaamuks@gmail.com>
2026-07-20 19:07:37 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 console.log("Добро пожаловать в Valto!");
+document.querySelectorAll(".chat").forEach(chat => {
+
+    chat.addEventListener("click", function (e) {
+
+        e.preventDefault();
+
+        document.body.classList.add("page-out");
+
+        const link = this.closest("a").href;
+
+        setTimeout(() => {
+            window.location.href = link;
+        }, 250);
+
+    });
+
+});
